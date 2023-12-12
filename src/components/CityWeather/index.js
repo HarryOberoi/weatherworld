270053e84c99475d0787a0809f0092c34d095caf
HarryOberoi <harpreetsoberoi85@gmail.com>
2023-12-13 01:58:57 +1100

    Pushing workflow yml file

--- src/components/CityWeather/index.js
+++ src/components/CityWeather/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./index.css";
 import CityDataLoading from "./CityDataLoading";
